2ffc1709e479ebd750035e824192011df50876ca
xuhuachao <xuhuachao@tianmengit.com>
2017-06-04 19:17:17 +0800

    test

--- ssm/src/test/java/com/aaron/ssm/test/TestMyBatis.java
+++ ssm/src/test/java/com/aaron/ssm/test/TestMyBatis.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.aaron.ssm.model.User;
@@ -15,7 +14,7 @@ import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
-public class TestMyBatis extends AbstractJUnit4SpringContextTests {
+public class TestMyBatis {
     private static Logger logger = Logger.getLogger(TestMyBatis.class);
     // private ApplicationContext ac = null;
     @Resource
